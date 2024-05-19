6cb728b88ca8e1f8adc83ce1a8f74b40cf0e389b
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2024-05-19 01:58:21 +0100

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -16,16 +16,27 @@ OS_PRETTY_NAME_VALUE=`rm_q $OS_PRETTY_NAME` #!
 #########################################################
 
 
-HOSTNAME_E=`cat /proc/sys/kernel/hostname` #!
-KERNEL_VERSION=`uname -r` #!
-SHELL_NAME=`echo -e "$SHELL"` #!
+ echo -e "\e[33m Operating System:\e[0m $(uname -o)"  #OS
+    echo -e "\e[33m Kernel Name:\e[0m $(uname -s)"  #Kernel
+    echo -e "\e[33m Kernel Version:\e[0m $(uname -r)"  #Kernel
+    echo -e "\e[33m Kernel Release:\e[0m $(uname -v)"  #Kernel
+    echo -e "\e[33m Kernel Architecture:\e[0m $(uname -m)"  #Kernel
+    echo -e "\e[33m\e[33m Machine Hardware Platform:\e[0m $(uname -i)"  # Machine Hardware Platform
+    echo -e "\e[33m Processor Type:\e[0m $(uname -p)"  # Processor Type
+    echo -e "\e[33m CPU:\e[0m $(grep "model name" /proc/cpuinfo | cut -d ' ' -f 3- | uniq)"  #CPU
+    echo -e "\e[33m GPU:\e[0m $(lspci | grep VGA | cut -d ':' -f 3 | cut -d '[' -f 1 | sed 's/^ *//')" #GPU
+    echo -e "\e[33m Memory:\e[0m $(free -h | awk '/^Mem/ {print $2 " total, " $3 " used"}')"  #Memory
+    echo -e "\e[33m Disk Usage:\e[0m $(df -h / | awk 'NR==2 {print $3 " used, " $4 " free"}')"  #Disk Usage
+    echo -e "\e[33m Second Hard Drive Usage:\e[0m $(df -h /dev/sda1 | awk 'NR==2 {print $3 " used, " $4 " free"}')"  #Disk Usage2
+    echo -e "\e[33m Flash Disk:\e[0m $(df -h /dev/sda | awk 'NR==2 {print $3 " used, " $4 " free"}')"  #Disk Usage3
+    echo -e "\e[33mClock/Time:\e[0m $(date +"%H:%M")" #time
+    echo -e "\e[33m Mounted Drive/Drives:\e[0m" #mount drive
+    mount | grep '^/dev/' | awk '{print $1 " mounted on " $3}'
 
 neofetch() {
     ASCII=`cat neofetch-ascii.txt`
     echo -e "$ASCII\n\n"
-    echo -e "             \e[32mOS: \e[0m$OS_PRETTY_NAME_VALUE\n"
-    echo -e "             \e[32mHostname: \e[0m$HOSTNAME_E\n"
-    echo -e "             \e[32mShell: \e[0m$SHELL_NAME\n"
+   
 }
 
 neofetch
